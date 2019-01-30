f19630372dd1b7f20e5485f9f968c549ba093dd4
Zevest <steveDP32014@gmail.com>
2019-01-30 19:55:37 +0100

    University Project
    Website that will store all university projects.

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+console.log("hello World");
+window.addEventListener("load",main);
+function main(){
+  h1 = document.createElement("H1");
+  h1.innerHTML = "Test"
+  document.body.appendChild(h1);
+}
